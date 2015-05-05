7f923b898796e48b8565563b7088bb0569f666a4
Nicholas Yeung <dmsnick88@gmail.com>
2015-05-04 23:28:09 -0400

    fixed spawning intervals and direction

--- Assets/Scripts/LoadFirstBatch.js
+++ Assets/Scripts/LoadFirstBatch.js
@@ -5,6 +5,7 @@
 var prefab : GameObject;
 var minOffset : float;
 var maxOffset : float;
+var XOffset : float;
 //var amount : int = 20;
 var xRadius : float = 5;
 var yRadius : float = 5;
@@ -17,7 +18,7 @@ var yRotation : float = 0;
 var zRotation : float = 0;
 
 private var timer = 0.0;
-//private var count = 0;
+private var randX : float;
 private var randY : float;
 
 function Update() {
@@ -27,11 +28,14 @@ function Update() {
 function Generate () {
 	if (timer >= timeOffset){
 	randY = Random.Range(minOffset,maxOffset);
-	//randZ = Random.Range(minOffset,maxOffset);
+	randX = Random.Range(-XOffset,XOffset);
 	//Instantiate(prefab, Vector3(transform.position.x - randX, transform.position.y ,  transform.position.z + randZ), Quaternion.Euler(-90, 0, 0));
-	Instantiate(prefab, Vector3(Random.insideUnitSphere.x * xRadius + transform.position.x, transform.position.y - randY, Random.insideUnitSphere.z * zRadius + transform.position.z), Quaternion.Euler(xRotation, yRotation, zRotation));
-	timer = 0;
+	//Instantiate(prefab, Vector3(Random.insideUnitSphere.x * xRadius - transform.position.x, transform.position.y - randY, transform.position.z), Quaternion.Euler(xRotation, yRotation, zRotation));
+	if (randX > 0) Instantiate(prefab, Vector3(transform.position.x - XOffset, transform.position.y - randY, transform.position.z), Quaternion.Euler(xRotation, yRotation, zRotation));
+	if (randX < 0) Instantiate(prefab, Vector3(transform.position.x + XOffset, transform.position.y - randY, transform.position.z), Quaternion.Euler(xRotation, yRotation, zRotation));
 	//count += 1;
+	//count += 1;
+	timer = 0;
 	}
 }
 
---
+++ Assets/Scripts/ObjMovement.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+var speed : float = 5.0;
+var initialPosition : Vector3;
+var moveSpeed : float = 20;
+
+var choice : boolean;
+//private var choiceBool = Random.value;
+
+function Start() {
+	initialPosition = transform.position;
+	randomBool();
+}
+
+// function randomBool (){
+// 	if (choiceBool > 0.5) choice = true;
+// 	else choice = false;
+// }
+
+function randomBool (){
+	if (transform.position.x < 0) choice = true;
+	else choice = false;
+}
+
+function Update () {
+	print (choice);
+	transform.position = Vector3.MoveTowards(transform.position, initialPosition, speed*Time.deltaTime);
+	if(choice == false)	transform.position += Vector3.left * Time.deltaTime * moveSpeed;
+	else transform.position += Vector3.right * Time.deltaTime * moveSpeed;
+}
