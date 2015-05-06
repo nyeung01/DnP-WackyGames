569b5a82543e8db780ab98fd7a0b8997b8aafa51
Damien <damie904@gmail.com>
2015-05-06 17:09:03 -0400

    Code Stuff

---
+++ Assets/ObjectBeGone.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var timer : float = 20;
+
+var increase : float = 0;
+function Start () {
+
+}
+
+function Update () {
+	//timer++;
+	//if(timer == 400)
+	//{
+	//GameObject.Destroy()
+	//}
+	
+	Destroy (gameObject, timer);
+}
--- Assets/Scripts/PlayerDies.js
+++ Assets/Scripts/PlayerDies.js
@@ -1,9 +1,13 @@
 ﻿#pragma strict
 
-var explosion : GameObject; 
+var explosion : GameObject[]; 
 
 function OnCollisionEnter (collision) {
 	Destroy(gameObject);
-	Instantiate(explosion,transform.position,Quaternion.Euler(-90,0,0));
-	explosion.GetComponent.<ParticleAnimator>().autodestruct = true;
+	//Instantiate(explosion,transform.position,Quaternion.Euler(-90,0,0));
+	//explosion.GetComponent.<ParticleAnimator>().autodestruct = true;
+	for(var i=0; i < explosion.length; i++){
+		Instantiate(explosion[i],transform.position,Quaternion.Euler(-90,0,0));
+	}
+
 }
